5a822c9717f33f5577a0f252c5ccb4e70fea5a28
muthukumaran8219 <hmsince2021@gmail.com>
2024-07-17 17:04:19 +0530

    invoice form

---
+++ demo.js
@@ -0,0 +1,11 @@
+// // function demo(num){
+// //     return num>1
+// // }
+// // console.log(demo(3))
+
+// function sayHello(){
+//     console.log("hello to everyone")
+// }
+// sayHello()
+// let output = sayHello()
+// document.getElementById("inpt").textContent = output;
